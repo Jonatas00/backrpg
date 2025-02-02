844dd10b9105fb58de8890a2307ad67ce16ee96c
jonatas <djonatas31@gmail.com>
2025-02-02 00:52:42 -0300

    .

--- src/main/java/com/github/prototipo_rpg/backrpg/BackrpgApplication.java
+++ src/main/java/com/github/prototipo_rpg/backrpg/BackrpgApplication.java
@@ -8,6 +8,8 @@ public class BackrpgApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackrpgApplication.class, args);
-	}
 
+
+
+	}
 }
---
+++ src/main/java/com/github/prototipo_rpg/backrpg/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.github.prototipo_rpg.backrpg.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserController {
+
+  @GetMapping("/teste")
+  public ResponseEntity getTestData() {
+    String phrase = "Hello World";
+    return ResponseEntity.status(401).body(phrase);
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity saveTestData(@RequestBody String name) {
+    if (name.equals("videiras")) {
+      return ResponseEntity.status(200).body("Olá " + name + " , tudo bem?");
+    } else {
+      return ResponseEntity.status(401).body("Você não é o videiras!!!!");
+    }
+  }
+}
