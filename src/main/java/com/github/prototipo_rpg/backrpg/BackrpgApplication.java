e0271e41e3bfb063de39a7888083fc57313f60df
jonatas <djonatas31@gmail.com>
2025-02-01 23:49:28 -0300

    .

---
+++ src/main/java/com/github/prototipo_rpg/backrpg/BackrpgApplication.java
@@ -0,0 +1,13 @@
+package com.github.prototipo_rpg.backrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackrpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackrpgApplication.class, args);
+	}
+
+}
