88a01e4d5d42fe30e43f4203f292912dd129cb77
jonatas00 <djonatas31@gmail.com>
2025-02-05 12:15:10 -0300

    add

--- src/main/java/com/github/prototipo_rpg/backrpg/BackrpgApplication.java
+++ src/main/java/com/github/prototipo_rpg/backrpg/BackrpgApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackrpgApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackrpgApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BackrpgApplication.class, args);
+  }
 }
