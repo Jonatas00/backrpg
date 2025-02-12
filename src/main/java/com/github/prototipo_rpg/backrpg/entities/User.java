14a23080c7de114d03c20769ed00ed8041f0454a
jonatas00 <djonatas31@gmail.com>
2025-02-12 12:27:55 -0300

    fix database

--- src/main/java/com/github/prototipo_rpg/backrpg/entities/User.java
+++ src/main/java/com/github/prototipo_rpg/backrpg/entities/User.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.lang.NonNull;
 
-@Entity(name = "tb_user")
+@Table(name = "tb_user2")
+@Entity
 @Getter
 @Setter
 public class User {
